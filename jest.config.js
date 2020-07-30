7d6ec56949d26f6e6c97c76d802f760ef38182bd
David de Boer <david@ddeboer.nl>
2020-07-30 18:14:45 +0200

    Validate datasets using SHACL

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,3 @@
-export default {
-  roots: ['test/'],
-  preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  collectCoverage: true,
-  coverageReporters: ['json-summary', 'text'],
-  coverageThreshold: {
-    global: {
-      lines: 97,
-      statements: 97.09,
-      branches: 100,
-      functions: 94.73,
-    },
-  },
-  transform: {},
+module.exports = {
+  preset: 'ts-jest',
 };
