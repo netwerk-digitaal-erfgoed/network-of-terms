3ac73662fae4e9e485f9082a7728bcb4b98f76dc
David de Boer <david@ddeboer.nl>
2020-08-01 10:27:34 +0200

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,4 +13,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
     ],
+    rules: {
+        '@typescript-eslint/semi': ['error', 'never'],
+    },
 };
