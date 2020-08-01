c3686b188aadc36dbf0247f8d5102f8ebbe8d889
David de Boer <david@ddeboer.nl>
2020-08-01 10:27:34 +0200

    Fix lint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    env: {
+        "jest": true,
+        "es6": true,
+        "node": true
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+        '@typescript-eslint/semi': ['error', 'never'],
+    },
+};
