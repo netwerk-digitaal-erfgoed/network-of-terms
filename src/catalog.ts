6d5a7aa5f6580058c7c57e3035b000f7051b09a4
David de Boer <david@ddeboer.nl>
2020-08-02 19:11:54 +0200

    Apply gts (#5)

--- src/catalog.ts
+++ src/catalog.ts
@@ -1,116 +1,117 @@
-import fs from 'fs'
-import RdfParser from 'rdf-parse'
-import * as RDF from 'rdf-js'
-import {newEngine} from '@comunica/actor-init-sparql-rdfjs'
-import {Bindings, IActorQueryOperationOutputBindings} from '@comunica/bus-query-operation'
-import {Transform, TransformCallback} from 'stream'
-import Path from 'path'
-import N3 = require('n3')
-import {URL} from 'url'
+import fs from 'fs';
+import RdfParser from 'rdf-parse';
+import * as RDF from 'rdf-js';
+import {newEngine} from '@comunica/actor-init-sparql-rdfjs';
+import {
+  Bindings,
+  IActorQueryOperationOutputBindings,
+} from '@comunica/bus-query-operation';
+import {Transform, TransformCallback} from 'stream';
+import Path from 'path';
+import N3 = require('n3');
+import {URL} from 'url';
 
 export class Catalog {
-    constructor(
-        readonly datasets: ReadonlyArray<Dataset>,
-    ) {
-    }
+  constructor(readonly datasets: ReadonlyArray<Dataset>) {}
 
-    public static async fromStore(store: RDF.Store): Promise<Catalog> {
-        const query = `
-            PREFIX schema: <http://schema.org/> 
-            SELECT * WHERE {
-                ?dataset a schema:Dataset ;
-                    schema:distribution ?distribution ;
-                    schema:name ?name .
-                ?distribution schema:contentUrl ?distributionUrl ;
-                    schema:potentialAction/schema:query ?query . 
-            }
-        `
-        const result = (await newEngine().query(query, {
-            sources: [{
-                type: 'rdfjsSource',
-                value: store
-            }]
-        })) as IActorQueryOperationOutputBindings
+  public static async fromStore(store: RDF.Store): Promise<Catalog> {
+    const query = `
+      PREFIX schema: <http://schema.org/> 
+        SELECT * WHERE {
+          ?dataset a schema:Dataset ;
+            schema:distribution ?distribution ;
+            schema:name ?name .
+          ?distribution schema:contentUrl ?distributionUrl ;
+            schema:potentialAction/schema:query ?query . 
+        }`;
+    const result = (await newEngine().query(query, {
+      sources: [
+        {
+          type: 'rdfjsSource',
+          value: store,
+        },
+      ],
+    })) as IActorQueryOperationOutputBindings;
 
-        const promise: Promise<Dataset[]> = new Promise((resolve, reject) => {
-            const datasets: Dataset[] = []
-            result.bindingsStream.on('data', (bindings: Bindings) => {
-                datasets.push(
-                    new Dataset(
-                        new URL(bindings.get('?dataset').value),
-                        bindings.get('?name').value,
-                        new Distribution(
-                            new URL(bindings.get('?distributionUrl').value),
-                            bindings.get('?query').value,
-                        ),
-                    )
-                )
-            })
-            result.bindingsStream.on('end', () => resolve(datasets))
-            result.bindingsStream.on('error', () => reject)
-        })
+    const promise: Promise<Dataset[]> = new Promise((resolve, reject) => {
+      const datasets: Dataset[] = [];
+      result.bindingsStream.on('data', (bindings: Bindings) => {
+        datasets.push(
+          new Dataset(
+            new URL(bindings.get('?dataset').value),
+            bindings.get('?name').value,
+            new Distribution(
+              new URL(bindings.get('?distributionUrl').value),
+              bindings.get('?query').value
+            )
+          )
+        );
+      });
+      result.bindingsStream.on('end', () => resolve(datasets));
+      result.bindingsStream.on('error', () => reject);
+    });
 
-        return new Catalog(await promise)
-    }
+    return new Catalog(await promise);
+  }
 }
 
 export class Dataset {
-    constructor(
-        readonly iri: IRI,
-        readonly name: string,
-        readonly distribution: Distribution,
-    ) {
-    }
+  constructor(
+    readonly iri: IRI,
+    readonly name: string,
+    readonly distribution: Distribution
+  ) {}
 }
 
 export class Distribution {
-    constructor(
-        readonly url: URL,
-        readonly query: string,
-    ) {
-    }
+  constructor(readonly url: URL, readonly query: string) {}
 }
 
-export type IRI = URL
+export type IRI = URL;
 
-function addStreamToStore(store: RDF.Store, stream: RDF.Stream): Promise<RDF.Store> {
-    return new Promise((resolve) => store.import(stream).once('end', () => resolve(store)))
+function addStreamToStore(
+  store: RDF.Store,
+  stream: RDF.Stream
+): Promise<RDF.Store> {
+  return new Promise(resolve =>
+    store.import(stream).once('end', () => resolve(store))
+  );
 }
 
 export async function fromFiles(directory: string): Promise<RDF.Store> {
-    const files = fs.readdirSync(directory)
-    const store = new N3.Store()
-    for (const file of files) {
-        const quadStream = RdfParser.parse(
-            fs.createReadStream(directory + '/' + file),
-            {path: file}
-        ).pipe(new InlineFiles())
-        await addStreamToStore(store, quadStream)
-    }
+  const files = fs.readdirSync(directory);
+  const store = new N3.Store();
+  for (const file of files) {
+    const quadStream = RdfParser.parse(
+      fs.createReadStream(directory + '/' + file),
+      {path: file}
+    ).pipe(new InlineFiles());
+    await addStreamToStore(store, quadStream);
+  }
 
-    return store
+  return store;
 }
 
 /**
  * An RDF.Quad transform that inlines file://... references in the quad's object value.
  */
 class InlineFiles extends Transform {
-    constructor() {
-        super({objectMode: true})
+  constructor() {
+    super({objectMode: true});
+  }
+
+  async _transform(
+    quad: RDF.Quad,
+    encoding: BufferEncoding,
+    callback: TransformCallback
+  ) {
+    if (quad.object.value.startsWith('file://')) {
+      const file = Path.resolve(quad.object.value.substr(7));
+      quad.object.value = await fs.promises.readFile(file, 'utf-8');
     }
 
-    async _transform(
-      quad: RDF.Quad,
-      encoding: BufferEncoding,
-      callback: TransformCallback
-    ) {
-        if (quad.object.value.startsWith('file://')) {
-            const file = Path.resolve(quad.object.value.substr(7))
-            quad.object.value = await fs.promises.readFile(file, 'utf-8')
-        }
+    this.push(quad, encoding);
 
-        this.push(quad, encoding)
-
-        callback()
-    }
+    callback();
+  }
 }
