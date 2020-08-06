469da24405c4935ba3b8dabfc2e44559047e46b2
David de Boer <david@ddeboer.nl>
2020-08-06 17:41:07 +0200

    Get datasets by identifier

--- test/catalog.test.ts
+++ test/catalog.test.ts
@@ -1,6 +1,12 @@
 import {Catalog, fromFiles} from '../src/catalog';
 
+let catalog: Catalog;
+
 describe('Catalog', () => {
+  beforeAll(async () => {
+    catalog = await Catalog.default();
+  });
+
   it('can be created from files', async done => {
     const store = await fromFiles('catalog/');
     const catalog = await Catalog.fromStore(store);
@@ -9,5 +15,16 @@ describe('Catalog', () => {
       expect.stringContaining('CONSTRUCT {')
     );
     done();
+  });
+
+  it('can list datasets', () => {
+    expect(catalog.datasets.length).toBe(6);
+  });
+
+  it('can retrieve datasets by identifier', () => {
+    expect(catalog.getByIdentifier('nope')).toBeUndefined();
+    // eslint-disable-next-line @typescript-eslint/no-extra-non-null-assertion
+    const cht = catalog.getByIdentifier('cht')!!;
+    expect(cht.identifier).toEqual('cht');
   });
 });
