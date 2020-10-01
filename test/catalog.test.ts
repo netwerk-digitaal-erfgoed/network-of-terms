584006c9e85911a735cc70d7c9a180d0f80dfaf4
Sjors de Valk <sdevalk@users.noreply.github.com>
2020-10-01 21:32:11 +0200

    feat: Add Muziekweb term sources (netwerk-digitaal-erfgoed/network-of-terms-catalog#23)

--- test/catalog.test.ts
+++ test/catalog.test.ts
@@ -8,7 +8,7 @@ describe('Catalog', () => {
   });
 
   it('can list datasets', () => {
-    expect(catalog.datasets.length).toBe(6);
+    expect(catalog.datasets.length).toBe(8);
   });
 
   it('can retrieve datasets by IRI', () => {
