09f91882b6c1a62b64de2e082351028c9cd50bdf
David de Boer <david@ddeboer.nl>
2021-07-06 17:29:35 +0200

    fix: URL type error
    ```
    TypeError [ERR_INVALID_ARG_TYPE]: The \"url\" argument must be of type
    string. Received an instance of IRI"}
    ```

--- src/services/query.ts
+++ src/services/query.ts
@@ -64,7 +64,7 @@ export class QueryTermsService {
       sources: [
         {
           type: 'sparql', // Only supported type for now
-          value: this.distribution.endpoint,
+          value: this.distribution.endpoint.toString(),
         },
       ],
       initialBindings: Bindings({
