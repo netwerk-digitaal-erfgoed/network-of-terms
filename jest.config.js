f6c5760dcf2066702fdddcdc90cc494e49db113e
David de Boer <david@ddeboer.nl>
2021-07-08 14:05:15 +0200

    Enforce code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@ module.exports = {
   roots: ['test/'],
   preset: 'ts-jest',
   testTimeout: 10000,
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 80,
+      statements: 80,
+      branches: 80,
+      functions: 80,
+    },
+  },
 };
