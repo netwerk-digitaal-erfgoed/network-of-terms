82093b6160374807369605e9400b07207ac1f655
David de Boer <david@ddeboer.nl>
2021-07-09 16:48:29 +0200

    Clean up

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import {server} from './server/server';
 (async () => {
   try {
     const httpServer = await server(await Catalog.default());
-    console.log('LISTENING');
     await httpServer.listen(3123, '0.0.0.0');
   } catch (err) {
     console.error(err);
