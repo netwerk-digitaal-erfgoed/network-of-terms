4c6da27f543d75519df3b58353a86ecaf4eb00fc
David de Boer <david@ddeboer.nl>
2022-02-10 10:24:32 +0100

    Add CHT

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import {server} from './server/server';
 (async () => {
   try {
     const httpServer = await server(await Catalog.default(), [
-      // Only a single reconciliation service for now.
+      // Only a couple of reconciliation services to begin with.
       'https://data.netwerkdigitaalerfgoed.nl/rkd/rkdartists/sparql',
+      'https://data.cultureelerfgoed.nl/PoolParty/sparql/term/id/cht',
     ]);
     await httpServer.listen(3123, '0.0.0.0');
   } catch (err) {
