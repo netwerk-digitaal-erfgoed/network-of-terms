24252d85c7f87253458f03cc79f8a744a7c2a135
David de Boer <david@ddeboer.nl>
2022-03-24 10:49:32 +0100

    fix: Calculate score per individual term
    Previously, we calculated a combined score per set of prefLabels and set
    of altLabels and took the max between the two.
    
    Change this to calculate score for each of the individual prefLabels and
    altLabels.

--- jest.config.js
+++ jest.config.js
@@ -15,9 +15,9 @@ export default {
   ],
   coverageThreshold: {
     global: {
-      lines: 88.96,
-      statements: 88.83,
-      branches: 92.15,
+      lines: 88.94,
+      statements: 88.81,
+      branches: 92.1,
       functions: 86.9,
     },
   },
