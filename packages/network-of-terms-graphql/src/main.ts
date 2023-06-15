591a3a5da04cec39376ee7d0ed70a8be662a73fe
David de Boer <david@ddeboer.nl>
2023-06-15 13:17:47 +0200

    chore: Fix deprecated use

--- packages/network-of-terms-graphql/src/main.ts
+++ packages/network-of-terms-graphql/src/main.ts
@@ -6,7 +6,7 @@ import {server} from './server';
     const httpServer = await server(
       await getCatalog(process.env.CATALOG_PATH || undefined)
     );
-    await httpServer.listen(3123, '0.0.0.0');
+    await httpServer.listen({port: 3123, host: '0.0.0.0'});
   } catch (err) {
     console.error(err);
   }
