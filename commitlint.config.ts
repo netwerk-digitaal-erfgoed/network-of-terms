5e08e8f585d0db207ef95f5a04ac803e0be767da
David de Boer <david@ddeboer.nl>
2024-06-03 10:47:28 +0200

    fix: Ignore Dependabot commit messages

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,4 +4,8 @@ export default {
     'body-max-line-length': [2, 'always', 150],
     'subject-case': [2, 'always', ['sentence-case', 'lower-case']],
   },
+  ignores: [
+    (message: string) =>
+      message.includes('Signed-off-by: dependabot[bot] <support@github.com>'),
+  ],
 };
